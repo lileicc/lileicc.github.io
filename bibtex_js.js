9d8f9ffb601d11daa8c64ee62a8674980032f99e
成翔 <chengxiang.1@bytedance.com>
2019-08-12 09:50:16 +0800

    change the source of moment.min.js

--- bibtex_js.js
+++ bibtex_js.js
@@ -1184,7 +1184,7 @@ if (!window.jQuery || !window.moment) {
         //Add moment to the webpage for dates
         var mo = document.createElement('script');
         mo.type = 'text/javascript';
-        mo.src = 'https://rawgit.com/moment/moment/2.22.2/min/moment.min.js';
+        mo.src = 'http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.7.0/moment.min.js';
         document.getElementsByTagName('head')[0].appendChild(mo);
     }
 
